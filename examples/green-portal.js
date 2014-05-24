3475a7d9f1e3ce8161d55fcfe49f21a29e3e6b97
Damon Oehlman <damon.oehlman@gmail.com>
2014-05-24 13:54:20 +1000

    Make the green-portal example, green

--- examples/green-portal.js
+++ examples/green-portal.js
@@ -2,5 +2,5 @@ var skyportal = require('../');
 var commands = skyportal.commands;
 
 skyportal.open(skyportal.find(), function(err, portal) {
-  skyportal.send(commands.color(0, 0, 0), portal);
+  skyportal.send(commands.color(0, 255, 0), portal);
 });
