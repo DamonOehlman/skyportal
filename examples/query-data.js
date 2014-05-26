6529bb865def8f8dbae3588b54451229b879acc7
Damon Oehlman <damon.oehlman@gmail.com>
2014-05-26 20:09:21 +1000

    Modified example trying to get things running

--- examples/query-data.js
+++ examples/query-data.js
@@ -3,28 +3,42 @@ var commands = skyportal.commands;
 var async = require('async');
 
 skyportal.open(skyportal.find(), function(err, portal) {
-  skyportal.send(commands.query(0x10, 1), portal, function(err, bytes) {
-    var lastResponse = [];
 
-    if (err) {
-      return;
-    }
+  function query(blockIdx, callback) {
+    skyportal.send(commands.query(0x01, blockIdx), portal, function(err) {
+      var lastResponse = [];
 
-    async.until(
-      function() {
-        return lastResponse[0] === 0x51;
-      },
-
-      function(callback) {
-        skyportal.read(portal, function(err, bytes) {
-          lastResponse = bytes || [];
-          callback(err);
-        });
-      },
-
-      function(err) {
-        console.log(err, lastResponse);
+      if (err) {
+        return;
       }
-    );
-  });
+
+      async.until(
+        function() {
+          return lastResponse[0] === 0x51;
+        },
+
+        function(callback) {
+          skyportal.read(portal, function(err, bytes) {
+            lastResponse = bytes || [];
+            callback(err);
+          });
+        },
+
+        function(err) {
+          if (lastResponse[1] === 0x01) {
+            err = new Error('Portal refused to provide query information');
+          }
+
+          callback(err, lastResponse);
+        }
+      );
+    });
+  }
+
+//   skyportal.send(commands.status(), portal, function(err) {
+//     skyportal.read(portal, function(err, bytes) {
+//     });
+//   });
+
+  async.timesSeries(64, query);
 });
